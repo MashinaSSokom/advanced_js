a9e6b0c343eed23e41bb5b28986f190d376662b1
MashinaSSokom <skltan@mail.ru>
2021-11-23 17:39:59 +0800

    add some base styles

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const goods = [
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
+    { title: 'Shoes', price: 250 },
+    { title: 'Shoes', price: 250 },
 ];
 
 const $goodsList = document.querySelector('.goods-list');
   
 const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+    return `<div class="goods-item card m-1"><h3>${title}</h3><p>Цена: ${price} руб</p></div>`;
 };
   
 const renderGoodsList = (list = goods) => {
@@ -16,7 +18,7 @@ const renderGoodsList = (list = goods) => {
             (item) =>  {
                 return renderGoodsItem(item)
             }
-        ).join();
+        ).join('');
 
     $goodsList.insertAdjacentHTML('beforeend', goodsList);
 }

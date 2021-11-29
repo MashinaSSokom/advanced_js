474a9ccd2e56ea410596c14bd6c25c626c7ba81b
MashinaSSokom <skltan@mail.ru>
2021-11-29 22:32:40 +0800

    add classes for goods

--- script.js
+++ script.js
@@ -1,28 +1,93 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Shoes', price: 250 },
-];
+// const goods = [
+//     { title: 'Shirt', price: 150 },
+//     { title: 'Socks', price: 50 },
+//     { title: 'Jacket', price: 350 },
+//     { title: 'Shoes', price: 250 },
+//     { title: 'Shoes', price: 250 },
+//     { title: 'Shoes', price: 250 },
+// ];
+//
+// const $goodsList = document.querySelector('.goods-list');
+//
+// const renderGoodsItem = ({ title, price }) => {
+//     return `<div class="goods-item card m-1"><h3>${title}</h3><p>Цена: ${price} руб</p></div>`;
+// };
+//
+// const renderGoodsList = (list = goods) => {
+//     let goodsList = list.map(
+//             (item) =>  {
+//                 return renderGoodsItem(item)
+//             }
+//         ).join('');
+//
+//     $goodsList.insertAdjacentHTML('beforeend', goodsList);
+// }
+//
+// renderGoodsList();
 
-const $goodsList = document.querySelector('.goods-list');
-  
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item card m-1"><h3>${title}</h3><p>Цена: ${price} руб</p></div>`;
-};
-  
-const renderGoodsList = (list = goods) => {
-    let goodsList = list.map(
-            (item) =>  {
-                return renderGoodsItem(item)
-            }
-        ).join('');
 
-    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    render() {
+        return `<div class="goods-item card m-1"><h3>${this.title}</h3><p>Цена: ${this.price} руб</p></div>`;
+    }
 }
-  
-renderGoodsList();
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').insertAdjacentHTML('afterbegin', listHtml);
+    }
+
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
 
 
+// #1 Добавьте пустые классы для Корзины товаров и Элемента корзины товаров.
+// Продумайте, какие методы понадобятся для работы с этими сущностями.
+// class Basket {
+//
+// }
+// class BasketItem {
+//
+// }
+
+
+
+// #2 Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
+
+// #3 Некая сеть фастфуда предлагает несколько видов гамбургеров:
+// a. Маленький (50 рублей, 20 калорий).
+// b. Большой (100 рублей, 40 калорий).
+// Гамбургер может быть с одним из нескольких видов начинок (обязательно):
+// a. С сыром (+10 рублей, +20 калорий).
+// b. С салатом (+20 рублей, +5 калорий).
+// c. С картофелем (+15 рублей, +10 калорий).
+// Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий).
+// Напишите программу, рассчитывающую стоимость и калорийность гамбургера.
+// Можно использовать примерную архитектуру класса со следующей страницы, но можно использовать и свою.

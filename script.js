52b738863fe41fdc0d8a1911733cf431de2120f9
MashinaSSokom <skltan@mail.ru>
2021-12-02 17:36:00 +0700

    change fetchGood method

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-// const goods = [
-//     { title: 'Shirt', price: 150 },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-//     { title: 'Shoes', price: 250 },
-//     { title: 'Shoes', price: 250 },
-// ];
-//
-// const $goodsList = document.querySelector('.goods-list');
-//
-// const renderGoodsItem = ({ title, price }) => {
-//     return `<div class="goods-item card m-1"><h3>${title}</h3><p>Цена: ${price} руб</p></div>`;
-// };
-//
-// const renderGoodsList = (list = goods) => {
-//     let goodsList = list.map(
-//             (item) =>  {
-//                 return renderGoodsItem(item)
-//             }
-//         ).join('');
-//
-//     $goodsList.insertAdjacentHTML('beforeend', goodsList);
-// }
-//
-// renderGoodsList();
+// https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses – адрес API;
+// /catalogData.json – получить список товаров;
+// /getBasket.json – получить содержимое корзины;
+// /addToBasket.json – добавить товар в корзину;
+// /deleteFromBasket.json – удалить товар из корзины.
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/'
 
 
 class GoodsItem {
@@ -43,12 +24,23 @@ class GoodsList {
     }
 
     fetchGoods() {
-        this.goods = [
-            {title: 'Shirt', price: 150},
-            {title: 'Socks', price: 50},
-            {title: 'Jacket', price: 350},
-            {title: 'Shoes', price: 250},
-        ];
+        // this.goods = [
+        //     {title: 'Shirt', price: 150},
+        //     {title: 'Socks', price: 50},
+        //     {title: 'Jacket', price: 350},
+        //     {title: 'Shoes', price: 250},
+        // ];
+        fetch(`${API_URL}catalogData.json`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((request) => {
+                this.goods = request.map(good => ({title: good.product_name, price: good.price}))
+                this.render();
+            })
+            .catch((err) => {
+                console.log(err.text)
+            })
     }
 
     render() {
@@ -78,11 +70,8 @@ class GoodsList {
 
 const list = new GoodsList();
 list.fetchGoods();
-list.render();
+// list.render();
 
-
-// #1 Добавьте пустые классы для Корзины товаров и Элемента корзины товаров.
-// Продумайте, какие методы понадобятся для работы с этими сущностями.
 
 class Basket {
     constructor() {
@@ -127,144 +116,133 @@ class BasketItem {
         // Логика генерации html-шаблона элемента
     }
 }
-
-
-// #3 Некая сеть фастфуда предлагает несколько видов гамбургеров:
-// a. Маленький (50 рублей, 20 калорий).
-// b. Большой (100 рублей, 40 калорий).
-// Гамбургер может быть с одним из нескольких видов начинок (обязательно):
-// a. С сыром (+10 рублей, +20 калорий).
-// b. С салатом (+20 рублей, +5 калорий).
-// c. С картофелем (+15 рублей, +10 калорий).
-// Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий).
-// Напишите программу, рассчитывающую стоимость и калорийность гамбургера.
-// Можно использовать примерную архитектуру класса со следующей страницы, но можно использовать и свою.
-
-class Hamburger {
-    constructor(size, stuffing) {
-        this.toppings = [
-            {
-                title: 'mayo',
-                price: 20,
-                caloric: 5,
-                count: 0
-            },
-            {
-                title: 'spice',
-                price: 15,
-                caloric: 0,
-                count: 0
-            },
-        ]
-        switch (size) {
-            case ('big'): {
-                this.size = {
-                    title: 'big',
-                    price: 100,
-                    caloric: 40
-                }
-                break
-            }
-            case ('small'): {
-                this.size = {
-                    title: 'small',
-                    price: 50,
-                    caloric: 20
-                }
-                break
-            }
-            default: throw new Error("Invalid hamburger size! Available sizes: big, small")
-        }
-        switch (stuffing) {
-            case ('cheese'): {
-                this.stuffing = {
-                    title: 'cheese',
-                    price: 10,
-                    caloric: 20
-                }
-                break
-            }
-            case ('salad'): {
-                this.stuffing = {
-                    title: 'salad',
-                    price: 20,
-                    caloric: 5
-                }
-                break
-            }
-            case ('fries'): {
-                this.stuffing = {
-                    title: 'fries',
-                    price: 15,
-                    caloric: 10
-                }
-                break
-            }
-            default:
-                throw new Error("Invalid hamburger stuffing! Available stuffings: cheese, salad, fries.")
-        }
-    }
-
-    addTopping(topping) {    // Добавить добавку
-        let msg = `Invalid topping! Available toppings: ${this.toppings.map(el => el.title)}`
-        this.toppings.forEach(el => {
-            if (el.title === topping) {
-                el.count += 1
-                msg = `Add one more ${topping}!`
-            }
-        })
-        console.log(msg)
-    }
-
-    removeTopping(topping) { // Убрать добавку
-        let msg = `Invalid topping! Available toppings: ${this.toppings.map(el => el.title)}`
-        this.toppings.forEach(el => {
-            if (el.title === topping) {
-                if (el.count === 0) {
-                    msg = `${topping} count is already zero ^_^`
-                } else {
-                    el.count -= 1
-                    msg = `Reduce quantity of ${topping} by 1!`
-                }
-
-            }
-        })
-        console.log(msg)
-    }
-
-    getToppings() {   // Получить список добавок
-        return this.toppings
-    }
-
-    getSize() {              // Узнать размер гамбургера
-        return this.size.title
-    }
-
-    getStuffing() {          // Узнать начинку гамбургера
-        return this.stuffing.title
-    }
-
-    calculatePrice() {       // Узнать цену
-        return this.size.price + this.stuffing.price + this.toppings.reduce((acc, cur) => acc+=cur.price*cur.count, 0)
-    }
-
-    calculateCalories() {    // Узнать калорийность
-        return this.size.caloric + this.stuffing.caloric + this.toppings.reduce((acc, cur) => acc+=cur.caloric*cur.count, 0)
-    }
-}
-
-let hamburger = new Hamburger('big', 'cheese')
-console.log(hamburger.getSize())
-console.log(hamburger.getStuffing())
-console.log(hamburger.getToppings())
-
-hamburger.addTopping('mayo')
-hamburger.addTopping('spice')
-console.log(hamburger.getToppings())
-
-hamburger.addTopping('mayo123123')
-hamburger.removeTopping('spice')
-console.log(hamburger.getToppings())
-
-console.log(hamburger.calculatePrice())
-console.log(hamburger.calculateCalories())
+//
+// class Hamburger {
+//     constructor(size, stuffing) {
+//         this.toppings = [
+//             {
+//                 title: 'mayo',
+//                 price: 20,
+//                 caloric: 5,
+//                 count: 0
+//             },
+//             {
+//                 title: 'spice',
+//                 price: 15,
+//                 caloric: 0,
+//                 count: 0
+//             },
+//         ]
+//         switch (size) {
+//             case ('big'): {
+//                 this.size = {
+//                     title: 'big',
+//                     price: 100,
+//                     caloric: 40
+//                 }
+//                 break
+//             }
+//             case ('small'): {
+//                 this.size = {
+//                     title: 'small',
+//                     price: 50,
+//                     caloric: 20
+//                 }
+//                 break
+//             }
+//             default:
+//                 throw new Error("Invalid hamburger size! Available sizes: big, small")
+//         }
+//         switch (stuffing) {
+//             case ('cheese'): {
+//                 this.stuffing = {
+//                     title: 'cheese',
+//                     price: 10,
+//                     caloric: 20
+//                 }
+//                 break
+//             }
+//             case ('salad'): {
+//                 this.stuffing = {
+//                     title: 'salad',
+//                     price: 20,
+//                     caloric: 5
+//                 }
+//                 break
+//             }
+//             case ('fries'): {
+//                 this.stuffing = {
+//                     title: 'fries',
+//                     price: 15,
+//                     caloric: 10
+//                 }
+//                 break
+//             }
+//             default:
+//                 throw new Error("Invalid hamburger stuffing! Available stuffings: cheese, salad, fries.")
+//         }
+//     }
+//
+//     addTopping(topping) {    // Добавить добавку
+//         let msg = `Invalid topping! Available toppings: ${this.toppings.map(el => el.title)}`
+//         this.toppings.forEach(el => {
+//             if (el.title === topping) {
+//                 el.count += 1
+//                 msg = `Add one more ${topping}!`
+//             }
+//         })
+//         console.log(msg)
+//     }
+//
+//     removeTopping(topping) { // Убрать добавку
+//         let msg = `Invalid topping! Available toppings: ${this.toppings.map(el => el.title)}`
+//         this.toppings.forEach(el => {
+//             if (el.title === topping) {
+//                 if (el.count === 0) {
+//                     msg = `${topping} count is already zero ^_^`
+//                 } else {
+//                     el.count -= 1
+//                     msg = `Reduce quantity of ${topping} by 1!`
+//                 }
+//
+//             }
+//         })
+//         console.log(msg)
+//     }
+//
+//     getToppings() {   // Получить список добавок
+//         return this.toppings
+//     }
+//
+//     getSize() {              // Узнать размер гамбургера
+//         return this.size.title
+//     }
+//
+//     getStuffing() {          // Узнать начинку гамбургера
+//         return this.stuffing.title
+//     }
+//
+//     calculatePrice() {       // Узнать цену
+//         return this.size.price + this.stuffing.price + this.toppings.reduce((acc, cur) => acc += cur.price * cur.count, 0)
+//     }
+//
+//     calculateCalories() {    // Узнать калорийность
+//         return this.size.caloric + this.stuffing.caloric + this.toppings.reduce((acc, cur) => acc += cur.caloric * cur.count, 0)
+//     }
+// }
+//
+// let hamburger = new Hamburger('big', 'cheese')
+// console.log(hamburger.getSize())
+// console.log(hamburger.getStuffing())
+// console.log(hamburger.getToppings())
+//
+// hamburger.addTopping('mayo')
+// hamburger.addTopping('spice')
+// console.log(hamburger.getToppings())
+//
+// hamburger.addTopping('mayo123123')
+// hamburger.removeTopping('spice')
+// console.log(hamburger.getToppings())
+//
+// console.log(hamburger.calculatePrice())
+// console.log(hamburger.calculateCalories())

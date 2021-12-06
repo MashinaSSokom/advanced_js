23f6fa6b845147f433df81d27a0c2c1474e5dbee
MashinaSSokom <skltan@mail.ru>
2021-12-06 23:01:33 +0800

    complete task 2

--- regexp.js
+++ regexp.js
@@ -1,9 +1,6 @@
-let converter = function (str) {
-    const regexp = /(\s)?'(\s|t|)/g
-    let result1 = str.replace(regexp, '$1'===''&&'$2'==='t'?`$1'$2`:`$1"$2`)
-    let result2 = str.match(regexp)
-    console.log(result1)
-    console.log(result2)
+let replacer = function (str) {
+    const regexp = /((\s|\w)?'([^t]|$))/g
+    return  str.replace(regexp, `$2"$3`)
 }
 
-converter("this is 'simple' string for 'test', but people aren't so smart'")
+console.log(replacer(`Someone said: 'Don't be stupid!'`))
